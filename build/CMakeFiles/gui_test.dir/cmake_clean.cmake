90b7ac80e87ac1bcd85ef9b90cffbfe4e696c692
Ronreacus Adams <131623836+Guidinglight23@users.noreply.github.com>
2026-02-25 18:12:37 +0000

    Done

---
+++ build/CMakeFiles/gui_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gui_test.dir/src/main.cpp.o"
+  "CMakeFiles/gui_test.dir/src/main.cpp.o.d"
+  "gui_test"
+  "gui_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gui_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
